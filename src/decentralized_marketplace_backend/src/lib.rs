7a6890c8d7e944a1031bab36f1740657e47b3d68
Stephen-Kimoi <moenisteve@gmail.com>
2024-03-24 12:29:42 +0300

    implementing items struct and serializing & deserializing them for storage

--- src/decentralized_marketplace_backend/src/lib.rs
+++ src/decentralized_marketplace_backend/src/lib.rs
@@ -1,4 +1,29 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+#[macro_use] 
+extern crate serde;
+use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable}; 
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager,VirtualMemory}; 
+use candid::{Decode, Encode, Principal};  
+use std::{borrow::Cow, cell::RefCell}; 
+
+type Memory = VirtualMemory<DefaultMemoryImpl>; 
+type IdCell = Cell<u64, Memory>; 
+
+// Items Struct 
+#[derive(candid::CandidType, Serialize, Deserialize, Default, Clone)] 
+struct Item {
+    id: u64, 
+    name: String, 
+    description: String
+} 
+
+// Serializing the items for storage and transmission 
+impl Storable for Item {
+   fn to_bytes(&self) -> Cow<[u8]> {
+       Cow::Owned(Encode!(self).unwrap())
+   }     
+
+   fn from_bytes(bytes: Cow<[u8]>) -> Self {
+       Decode!(bytes.as_ref(), Self).unwrap()
+   }
 }
+
